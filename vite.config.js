c34115017aad98c3fb4a9ff565ba9f1a8968853a
Chi.Nguyen <mr.nhchi@gmail.com>
2025-10-03 19:58:52 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ export default defineConfig({
         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://unpkg.com; " +
         "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
         "connect-src 'self' http://127.0.0.1:5001 https://www.googleapis.com https://cdn.jsdelivr.net ws:; " +
-        "img-src 'self' data: https://lh3.googleusercontent.com; " +
         "img-src 'self' data: blob: https://lh3.googleusercontent.com; " + 
         "frame-src 'self' https://drive.google.com; " +
         // SỬA LỖI: Đảm bảo có chỉ thị worker-src để thư viện AI hoạt động
