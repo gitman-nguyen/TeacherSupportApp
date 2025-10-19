d10333d4654d487474511df873ff636611195203
Chi.Nguyen <mr.nhchi@gmail.com>
2025-10-19 16:50:04 +0700

    fix constans.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'http://localhost:5001';
+export const BACKEND_URL = '/api';
 
 export const dayOfWeekMap = { '1': 'T2', '2': 'T3', '3': 'T4', '4': 'T5', '5': 'T6', '6': 'T7', '7': 'CN' };
 
